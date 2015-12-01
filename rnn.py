585d375ce0820f72dd346ed9841879b285137c1d
AlexPiche <AlexPiche@wpa181068.wireless.mcgill.ca>
2015-12-01 12:40:23 -0500

    minimal working RNN

--- rnn.py
+++ rnn.py
@@ -1,159 +1,159 @@
-# Based on https://github.com/craffel/Lasagne-tutorial
-from __future__ import print_function
-import pdb
-from load_data import load_subjects
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn import cross_validation
-import numpy as np
-patients = ['Dog_1','Dog_2','Dog_3','Dog_4','Dog_5','Patient_1']
-
-#you can load all the subjects you want by putting them in a list
-#the return value is a dictionary with the subjects as keys.
-all_subjects_dict = load_subjects(patients)
-
-#pat dict is a dictionary that has the different preprocessing techniques as its keys 
-pat1_dict = all_subjects_dict['Patient_1']
-
-#the different preprocessing techniques, michael hills created a classifier for each and then did an averag
-
-#the data itself is separated by whether it was interictal or preictal
-correlation_data_interictal, correlation_data_preictal = pat1_dict['corr']
-
-#classifiers = [LinearRegression(),LogisticRegression()]
-
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-Recurrent network example.  Trains a bidirectional vanilla RNN to output the
-sum of two numbers in a sequence of random numbers sampled uniformly from
-[0, 1] based on a separate marker sequence.
-'''
-
-
-
-import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
+import numpy as np
+import sklearn.datasets
+import os
+import matplotlib.pyplot as plt
+
+# Recurrent Networks
+
+# Our LSTM will have 10 hidden/cell units
+N_HIDDEN = 10
+N_FEATURES_DIM = 150
 
 
-# Min/max sequence length
-MIN_LENGTH = 50
-MAX_LENGTH = 1280
-# Number of units in the hidden (recurrent) layer
-N_HIDDEN = 100
-# Number of training sequences in each batch
-N_BATCH = 504
-n_batch = 100
+l_in = lasagne.layers.InputLayer(shape=(None, None, N_FEATURES_DIM))
+# This input will be used to provide the network with masks.
+# Masks are expected to be matrices of shape (n_batch, n_time_steps);
+# both of these dimensions are variable for us so we will use
+# an input shape of (None, None)
+l_mask = lasagne.layers.InputLayer(shape=(None, None))
 
-# Optimization learning rate
-LEARNING_RATE = .001
-# All gradients above this will be clipped
-GRAD_CLIP = 100
-# How often should we check the output?
-EPOCH_SIZE = 100
-# Number of epochs to train the net
+
+# All gates have initializers for the input-to-gate and hidden state-to-gate
+# weight matrices, the cell-to-gate weight vector, the bias vector, and the nonlinearity.
+# The convention is that gates use the standard sigmoid nonlinearity,
+# which is the default for the Gate class.
+print("Building the model")
+gate_parameters = lasagne.layers.recurrent.Gate(
+    W_in=lasagne.init.Orthogonal(), W_hid=lasagne.init.Orthogonal(),
+    b=lasagne.init.Constant(0.))
+
+cell_parameters = lasagne.layers.recurrent.Gate(
+    W_in=lasagne.init.Orthogonal(), W_hid=lasagne.init.Orthogonal(),
+    # Setting W_cell to None denotes that no cell connection will be used.
+    W_cell=None, b=lasagne.init.Constant(0.),
+    # By convention, the cell nonlinearity is tanh in an LSTM.
+    nonlinearity=lasagne.nonlinearities.tanh)
+
+
+# Bidirectional LSTM
+
+l_lstm = lasagne.layers.recurrent.LSTMLayer(
+    l_in, N_HIDDEN,
+    # We need to specify a separate input for masks
+    mask_input=l_mask,
+    # Here, we supply the gate parameters for each gate
+    ingate=gate_parameters, forgetgate=gate_parameters,
+    cell=cell_parameters, outgate=gate_parameters,
+    # We'll learn the initialization and use gradient clipping
+    learn_init=True, grad_clipping=100.)
+
+l_lstm_back = lasagne.layers.recurrent.LSTMLayer(
+    l_in, N_HIDDEN, ingate=gate_parameters,
+    mask_input=l_mask, forgetgate=gate_parameters,
+    cell=cell_parameters, outgate=gate_parameters,
+    learn_init=True, grad_clipping=100., backwards=True)
+
+# We'll combine the forward and backward layer output by summing.
+# Merge layers take in lists of layers to merge as input.
+l_sum = lasagne.layers.ElemwiseSumLayer([l_lstm, l_lstm_back])
+
+# First, retrieve symbolic variables for the input shape
+n_batch, n_time_steps, n_features = l_in.input_var.shape
+# Now, squash the n_batch and n_time_steps dimensions
+l_reshape = lasagne.layers.ReshapeLayer(l_sum, (-1, N_HIDDEN))
+# Now, we can apply feed-forward layers as usual.
+# We want the network to predict a single value, the sum, so we'll use a single unit.
+l_dense = lasagne.layers.DenseLayer(
+    l_reshape, num_units=1, nonlinearity=lasagne.nonlinearities.tanh)
+# Now, the shape will be n_batch*n_timesteps, 1.  We can then reshape to
+# n_batch, n_timesteps to get a single value for each timstep from each sequence
+l_out = lasagne.layers.ReshapeLayer(l_dense, (n_batch, n_time_steps))
+
+
+
+# Symbolic variable for the target network output.
+# It will be of shape n_batch, because there's only 1 target value per sequence.
+target_values = T.vector('target_output')
+# This matrix will tell the network the length of each sequences.
+# The actual values will be supplied by the gen_data function.
+mask = T.matrix('mask')
+
+# lasagne.layers.get_output produces an expression for the output of the net
+network_output = lasagne.layers.get_output(l_out)
+# The value we care about is the final value produced for each sequence
+# so we simply slice it out.
+predicted_values = network_output[:, -1]
+# Our cost will be mean-squared error
+cost = T.mean((predicted_values - target_values)**2)
+# Retrieve all parameters from the network
+all_params = lasagne.layers.get_all_params(l_out)
+# Compute adam updates for training
+updates = lasagne.updates.adam(cost, all_params)
+# Theano functions for training and computing cost
+train = theano.function(
+    [l_in.input_var, target_values, l_mask.input_var],
+    cost, updates=updates)
+compute_cost = theano.function(
+    [l_in.input_var, target_values, l_mask.input_var], cost)
+
+# We'll use this "validation set" to periodically check progress
+
+from load_data import load_subjects, load_subjects_test
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.svm import SVC
+from sklearn import cross_validation
+
+from Utilities import make_csv_predictions,train_predict
+
+import numpy as np
+
+print("Getting the data")
+all_patients = ['Dog_1','Dog_2','Dog_3','Dog_4','Dog_5','Patient_1','Patient_2']
+
+""" you can load all the subjects you want by putting them in a list
+	the return value is a dictionary with the subjects as keys """
+all_subjects_dict = load_subjects(all_patients)
+
+all_subjects_dict_test = load_subjects_test(all_patients)
+
+""" pat dict is a dictionary that has the different preprocessing techniques as its keys """
+pat1_dict = all_subjects_dict['Patient_1']
+
+pat1_dict_test = all_subjects_dict_test['Patient_1']
+
+""" the different preprocessing techniques, michael hills created a classifier for each and then did an average """
+#print pat1_dict.keys()
+
+"""	the data itself is separated by whether it was interictal or preictal """
+correlation_data_interictal, correlation_data_preictal = pat1_dict['corr']
+
+i, p = pat1_dict['fft_mag_fbin-mean']
+X = np.vstack((i, p))
+ones = np.ones(p.shape[0])
+zeros = np.zeros(i.shape[0])
+
+y = np.append(zeros,ones)
+
+mask = np.ones((X.shape[0], X.shape[2]))
+
+""" Loop over all patients, 
+	make probabilistic predictions for each method within a given patient
+	combine the probalities by either:
+	1) just summing them up
+	2) subtracting the mean prediction probability from each class 
+	   for each method and then summing (Avoids all 0 predictions) """
+
+# We'll train the network with 10 epochs of 100 minibatches each
 NUM_EPOCHS = 10
+EPOCH_SIZE = 100
+print("Beginning training")
+for epoch in range(NUM_EPOCHS):
+    for _ in range(EPOCH_SIZE):
+        train(X, y, mask)
+    cost_val = compute_cost(X, y, mask)
+    print("Epoch {} validation cost = {}".format(epoch + 1, cost_val))
 
 
-def main(num_epochs=NUM_EPOCHS):
-        print("Building network ...")
-        # First, we build the network, starting with an input layer
-        # Recurrent layers expect input of shape
-        # (batch size, max sequence length, number of features)
-        l_in = lasagne.layers.InputLayer(shape=(N_BATCH, MAX_LENGTH))
-        # The network also needs a way to provide a mask for each sequence.  We'll
-        # use a separate input layer for that.  Since the mask only determines
-        # which indices are part of the sequence for each batch entry, they are
-        # supplied as matrices of dimensionality (N_BATCH, MAX_LENGTH)
-        l_mask = lasagne.layers.InputLayer(shape=(N_BATCH, MAX_LENGTH))
-        # We're using a bidirectional network, which means we will combine two
-        # RecurrentLayers, one with the backwards=True keyword argument.
-        # Setting a value for grad_clipping will clip the gradients in the layer
-        l_forward = lasagne.layers.RecurrentLayer(
-            l_in, N_HIDDEN, mask_input=l_mask, grad_clipping=GRAD_CLIP,
-            W_in_to_hid=lasagne.init.HeUniform(),
-            W_hid_to_hid=lasagne.init.HeUniform(),
-            nonlinearity=lasagne.nonlinearities.tanh)
-        l_backward = lasagne.layers.RecurrentLayer(
-            l_in, N_HIDDEN, mask_input=l_mask, grad_clipping=GRAD_CLIP,
-            W_in_to_hid=lasagne.init.HeUniform(),
-            W_hid_to_hid=lasagne.init.HeUniform(),
-            nonlinearity=lasagne.nonlinearities.tanh, backwards=True)
-        # The objective of this task depends only on the final value produced by
-        # the network.  So, we'll use SliceLayers to extract the LSTM layer's
-        # output after processing the entire input sequence.  For the forward
-        # layer, this corresponds to the last value of the second (sequence length)
-        # dimension.
-        l_forward_slice = lasagne.layers.SliceLayer(l_forward, -1, 1)
-        # For the backwards layer, the first index actually corresponds to the
-        # final output of the network, as it processes the sequence backwards.
-        l_backward_slice = lasagne.layers.SliceLayer(l_backward, 0, 1)
-        # Now, we'll concatenate the outputs to combine them.
-        l_sum = lasagne.layers.ConcatLayer([l_forward_slice, l_backward_slice])
-        # Our output layer is a simple dense connection, with 1 output unit
-        l_out = lasagne.layers.DenseLayer(
-            l_sum, num_units=1, nonlinearity=lasagne.nonlinearities.tanh)
-
-        target_values = T.vector('target_output')
-
-        # lasagne.layers.get_output produces a variable for the output of the net
-        network_output = lasagne.layers.get_output(l_out)
-        # The value we care about is the final value produced for each sequence
-        predicted_values = network_output[:, -1]
-        # Our cost will be mean-squared error
-        cost = T.mean((predicted_values - target_values)**2)
-        # Retrieve all parameters from the network
-        all_params = lasagne.layers.get_all_params(l_out)
-        # Compute SGD updates for training
-        print("Computing updates ...")
-        updates = lasagne.updates.adagrad(cost, all_params, LEARNING_RATE)
-        # Theano functions for training and computing cost
-        print("Compiling functions ...")
-        train = theano.function([l_in.input_var, target_values, l_mask.input_var],
-                            cost, updates=updates)
-        compute_cost = theano.function(
-            [l_in.input_var, target_values, l_mask.input_var], cost)
-
-        # We'll use this "validation set" to periodically check progress
-
-        print("Training ...")
-        try:
-                for epoch in range(num_epochs):
-                        for _ in range(EPOCH_SIZE):
-                                for patient in patients:
-
-                                        cur_patient_data = all_subjects_dict[patient]
-
-                                        for method in cur_patient_data:
-            
-                                                i,p = cur_patient_data[method]
-                                                shape_i = i.shape
-                                                shape_p = p.shape
-
-                                                #change the shape from (#of examples,#channels,#time) to (#examples,channels*time)
-                                                interictal = np.reshape(i,(shape_i[0],np.product(shape_i[1:])))
-                                                preictal = np.reshape(p,(shape_p[0],np.product(shape_p[1:])))
-
-                                                X = np.vstack((interictal,preictal))
-
-                                                ones = np.ones(preictal.shape[0])
-                                                zeros = np.zeros(interictal.shape[0])
-
-                                                y = np.append(zeros,ones)
-                                                mask = np.ones(X.shape)
-
-                                                X.astype(theano.config.floatX)
-                                                y.astype(theano.config.floatX)
-                                                mask.astype(theano.config.floatX)
-                                                pdb.set_trace()
-
-                                                train(X, y, mask)
-                                                cost_val = compute_cost(X, y, mask)
-                                                print("Epoch {} validation cost = {}".format(epoch, cost_val))
-        except KeyboardInterrupt:
-                pass
-
-if __name__ == '__main__':
-        main()
