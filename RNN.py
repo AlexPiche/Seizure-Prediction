db58feb6c5efd4cb69245b289cbbcfe7344e034b
AlexPiche <AlexPiche@wpa181031.wireless.mcgill.ca>
2015-12-02 16:29:44 -0500

    damn RNN

--- RNN.py
+++ RNN.py
@@ -1,3 +1,4 @@
+import pdb
 import lasagne
 from lasagne.layers import *
 from lasagne.layers.recurrent import *
@@ -14,6 +15,23 @@ from nolearn.lasagne import NeuralNet
 
 import numpy as np
 
+gate_parameters = lasagne.layers.recurrent.Gate(
+        W_in=lasagne.init.Orthogonal(), W_hid=lasagne.init.Orthogonal(),
+        b=lasagne.init.Constant(0.))
+
+cell_parameters = lasagne.layers.recurrent.Gate(
+        W_in=lasagne.init.Orthogonal(), W_hid=lasagne.init.Orthogonal(),
+        # Setting W_cell to None denotes that no cell connection will be used.
+    W_cell=None, b=lasagne.init.Constant(0.),
+        # By convention, the cell nonlinearity is tanh in an LSTM.
+    nonlinearity=lasagne.nonlinearities.tanh)
+
+#l_in = lasagne.layers.InputLayer(shape=(None, None, N_FEATURES_DIM))
+#l_mask = lasagne.layers.InputLayer(shape=(None, None))
+
+# LSTM number of hidden/cell units
+N_HIDDEN = 10
+
 def float32(k):
         return np.cast['float32'](k)
 
@@ -23,66 +41,68 @@ def formatData(X, y = None):
         X = X.reshape(X.shape[0],1,X.shape[1],X.shape[2])
         X = X.astype(np.float32)
         if y is not None:
-            y = y.astype(np.int32)
+                y = y.astype(np.int32)
         return X,y
 
 
 class RNN:
     def __init__(self):
-        self.convnet = None
+            self.recnet = None
 
     def make_rnn(self,X,y):
-        FSIZE = (int(np.floor(X.shape[2]/4)), int(np.floor(X.shape[3]/4)))
+            recnet = NeuralNet(
+                    layers = [
+                            ('input', InputLayer ),
+                            ('lstm_forward', LSTMLayer),
+                            ('lstm_backward', LSTMLayer, {'backwards': True}),
+                            ('concat', ConcatLayer),
+                            ('lstm_sum', ElemwiseSumLayer),
+                            (ReshapeLayer, {"shape": (-1, N_HIDDEN)}),
+                            (DenseLayer, {'num_units': 1, 'nonlinearity': tanh}),
+                    ],
 
-        recnet = NeuralNet(
-            layers = [
-                ('input', InputLayer ),
+                    input_shape= (None, None, X.shape[2]),
 
-                ('lstm_forward', LSTMLayer),
-                ('lstm_backward', LSTMLayer),
+                    # LSTM parameters
+                    lstm_forward_incoming = 'input',
+                    lstm_forward_num_units = N_HIDDEN,
+                    lstm_forward_ingate=gate_parameters,
+                    lstm_forward_forgetgate=gate_parameters,
+                    lstm_forward_cell=cell_parameters,
+                    lstm_forward_outgate=gate_parameters,
+                    lstm_forward_learn_init=True,
+                    lstm_forward_grad_clipping=100.0,
+                    lstm_backward_incoming = 'input',
+                    lstm_backward_num_units = N_HIDDEN,
+                    lstm_backward_ingate=gate_parameters,
+                    lstm_backward_backgetgate=gate_parameters,
+                    lstm_backward_cell=cell_parameters,
+                    lstm_backward_outgate=gate_parameters,
+                    lstm_backward_learn_init=True,
+                    lstm_backward_grad_clipping=100.0,
+                    #lstm_backward_backwards=True,
 
-                ('concat', ConcatLayer),
+                    concat_incomings = ['lstm_forward', 'lstm_backward'],
+                    lstm_sum_incoming = 'concat',
 
-                ('lstm_sum', ElementwiseSumLayer),
-
-                (Reshape),
-
-                (DenseLayer, {'num_units': 1, 'nonlinearity': tanh}),
-            ],
-
-            input_shape= (None, 1, X.shape[2], X.shape[3]),
-
-            conv1_num_filters = NUM_FILTERS1,
-            conv1_filter_size = FSIZE , 
-            conv1_pad =  1,
-
-            conv2_num_filters = NUM_FILTERS2,
-            conv2_filter_size = FSIZE , 
-            conv2_pad =  1,
-
-            lstm_forward_incoming = 'input',
-            lstm_backward_incoming = 'input',
-            concat_incomings = ['lstm_forward', 'lstm_backward'],
-            lstm_sum_incoming = 'concat',
-
-            update_learning_rate=theano.shared(float32(0.01)),
-            update_momentum=theano.shared(float32(0.9)),
-            verbose=2,
-            max_epochs = 50,
-
+                    update_learning_rate=theano.shared(float32(0.01)),
+                    update_momentum=theano.shared(float32(0.9)),
+                    verbose=2,
+                    max_epochs = 50,
             )
-        return recnet
+            return recnet
 
     def fit(self,X,y):
-        X,y = formatData(X,y)
-        print X.shape
-        self.recnet = self.make_rnn(X,y)
-        self.recnet.fit(X,y)
+            X,y = formatData(X,y)
+            print X.shape
+            print y.shape
+            self.recnet = self.make_rnn(X,y)
+            self.recnet.fit(X,y)
 
     def predict_proba(self,X):
-        X,_ = formatData(X)
-        return self.recnet.predict_proba(X)
+            X,_ = formatData(X)
+            return self.recnet.predict_proba(X)
 
     def predict(self,X):
-        X,_ = formatData(X)
-        return self.recnet.predict(X)
+            X,_ = formatData(X)
+            return self.recnet.predict(X)
